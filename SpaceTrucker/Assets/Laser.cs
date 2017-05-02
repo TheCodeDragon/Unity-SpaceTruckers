1dde7bd34ff1a2da37e7691279556e044722ef89
TheCodeDragon <luke.duncumb@gmail.com>
2017-05-02 04:06:39 +0100

    Commit 2
    LINE RENDERING AND RAYCASTING

---
+++ SpaceTrucker/Assets/Laser.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+    //Laser?
+
+    private LineRenderer line;
+    public float fl_range;
+	// Use this for initialization
+	void Start () {
+        line = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit mHit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out mHit))
+        {
+            if (mHit.collider)
+            {
+                line.SetPosition(1, new Vector3(0, 0, mHit.distance));
+                Debug.Log("hit distance: "+ mHit.distance);
+            }
+        }
+        else
+        {
+            line.SetPosition(1, new Vector3(0, 0, fl_range));
+        }
+        
+	}
+}
---
+++ SpaceTrucker/Assets/raycastBasics.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class raycastBasics : MonoBehaviour {
+    //Basic Raycasting Script - Modify for shooting/mining
+
+    //Sticking these here for later
+    [Header("WeaponOptions")]
+    public float fl_Damage = 10;
+    public string st_DamageType;
+    public float fl_Range = 100f;
+
+    [Header("Raycasting")]
+    public Camera cam_Raycast;
+	// Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetButton("Fire1"))
+        {
+            CastRay();
+        }
+
+	}
+    public void CastRay()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(cam_Raycast.transform.position, cam_Raycast.transform.forward, out hit, fl_Range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+    void Shoot()
+    {
+       
+    }
+}
